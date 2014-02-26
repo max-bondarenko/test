107335429bdb5762c609a858ffbeb4fd9a89a30f
max-bondarenko <sy_s@ukr.net>
2014-02-26 15:41:38 +0200

    add maven project

---
+++ scala_maven/src/main/scala/org/max/RichOperations.scala
@@ -0,0 +1,13 @@
+package org.max
+
+/**
+ * Created by mbondarenko on 08.02.14.
+ */
+object RichOperations extends App {
+
+  println(1 max 5)
+
+  println(  0 until(10,2) )
+  println( 0 to (10,2) )
+
+}
