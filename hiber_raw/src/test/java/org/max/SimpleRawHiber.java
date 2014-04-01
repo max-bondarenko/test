bc5e14ecb0602bb94c671848f0f303a6afe8c123
max-bondarenko <sy_s@ukr.net>
2014-04-01 17:40:41 +0300

    add with Hall ElementCollection

--- hiber_raw/src/test/java/org/max/SimpleRawHiber.java
+++ hiber_raw/src/test/java/org/max/SimpleRawHiber.java
@@ -3,12 +3,17 @@ package org.max;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.Before;
 import org.junit.Test;
 import org.max.entity.Author;
 import org.max.entity.Book;
+import org.max.entity.Hall;
+import org.max.entity.Line;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
@@ -43,7 +48,34 @@ public class SimpleRawHiber {
         assertNotNull(list);
         assertEquals(1, list.size());
         assertEquals(new Integer(2), list.get(0).getId());
+    }
 
+    @Test
+    public void testHall() throws Exception {
+        Hall hall = new Hall();
+        hall.setName("Left Hall");
 
+        Session s = sf.openSession();
+        Transaction tx = s.beginTransaction();
+
+        System.out.println("is active :" + tx.isActive());
+        s.save(hall);
+
+        tx.commit();
+        System.out.println("is active :" + tx.isActive());
+
+        tx = s.beginTransaction();
+        hall.setTraditionalNames(new HashSet<String>());
+        hall.getTraditionalNames().add("lefthall");
+        hall.getTraditionalNames().add("lifthall");
+
+        tx.commit();
+        s.close();
+
+        s = sf.openSession();
+
+        Hall loaded = (Hall)s.byId(Hall.class).load(1);
+
+        assertEquals(2, loaded.getTraditionalNames().size());
     }
 }
