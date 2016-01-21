113f5905e2fb653ee45d1e16a646da788afc0c48
Maksym_Bondarenko@epam.com <Maksym_Bondarenko@epam.com>
2016-01-21 11:04:24 +0200

    added Spring Boot sample

---
+++ boot/src/main/java/org/max/boot/ctrl/SimpleController.java
@@ -0,0 +1,30 @@
+package org.max.boot.ctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+* Created by : Maksym_Bondarenko
+* Created at : 21-01-2016
+*/
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hetHello(){
+        return "Helo";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleController.class,args);
+    }
+
+
+
+}
