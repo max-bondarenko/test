77a9c2c2508540e3e85d8c55855da79ade1ac54e
Maksym_Bondarenko@epam.com <Maksym_Bondarenko@epam.com>
2016-06-09 11:54:52 +0300

    boot

--- boot/src/main/java/org/max/boot/ctrl/SimpleController.java
+++ boot/src/main/java/org/max/boot/ctrl/SimpleController.java
@@ -1,17 +1,20 @@
 package org.max.boot.ctrl;
 
+import org.max.boot.model.ExampleDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
 * Created by : Maksym_Bondarenko
 * Created at : 21-01-2016
 */
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class SimpleController {
 
     @RequestMapping("/")
@@ -20,8 +23,9 @@ public class SimpleController {
         return "Helo";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SimpleController.class,args);
+    @RequestMapping("/example")
+    @ResponseBody
+    public ExampleDto retExampleDto(){
+        return new ExampleDto("val",new Date(),11231212L);
     }
-
 }
